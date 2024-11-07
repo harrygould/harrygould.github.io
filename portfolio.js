64aadd58e568a0baf9632f627beeb465f522b849
harrygould <harry.gould95@gmail.com>
2024-11-08 10:48:04 +1100

    change of nav title to quotes

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,21 @@
-alert("JavaScript is Working!");
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+// alert("JavaScript is Working!");
+// document
+//   .getElementById("contactForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     const name = document.getElementById("name").value;
+//     const email = document.getElementById("email").value;
+//     const message = document.getElementById("message").value;
 
-    if (name && email && message) {
-      document.getElementById("confirmationMessage").innerText =
-        "Thank you for your message!";
-      document.getElementById("contactForm").reset();
-    } else {
-      document.getElementById("confirmationMessage").innerText =
-        "Please fill out all fields.";
-    }
-  });
+//     if (name && email && message) {
+//       document.getElementById("confirmationMessage").innerText =
+//         "Thank you for your message!";
+//       document.getElementById("contactForm").reset();
+//     } else {
+//       document.getElementById("confirmationMessage").innerText =
+//         "Please fill out all fields.";
+//     }
+//   });
 
 // // pacman game code!!!
 // const canvas = document.getElementById("pacmanCanvas");
